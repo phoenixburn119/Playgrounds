c49d836cedcd83a7bca8dfaf97166e3418557f1b
Adam Kinzer <adamkinzer915@gmail.com>
2015-12-03 13:26:36 -0700

    Created playground repo

---
+++ Java Swift Playground.playground/Contents.swift
@@ -0,0 +1,47 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var A = String()
+var B = Int()
+var C = Bool()
+
+
+
+A = "Hello from the otherside"
+B = 10
+C = false
+
+
+print(A)
+print(B)
+print(C)
+
+
+
+var AB = 10
+var BC = 9
+var CD = 1
+
+
+if (AB < CD)
+{
+    print("yay")
+}
+else (
+
+else if (BC == AB)
+{
+    print("BC equals AB")
+}
+else if (AB > CD)
+{
+    print("AB is greater than CD!")
+    AB-=10
+}
+
+if(AB < CD)
+{
+    print("AB sucks now")
+}
+
---
+++ Playground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var someVariable : Int
+someVariable = 7181
+someVariable = 3423
+var isName :Bool
+isName = false
