55ae7b7f386daeb87fe843bf6712dc6b35c9dbfc
Adam Kinzer <adamkinzer915@gmail.com>
2015-12-03 13:37:33 -0700

    Added code to playground.

---
+++ Tutorial Playground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Explit declaration of a String.
+let AFTERNOON_PLAYGROUND :String = "Learning swift"
+var myWords = AFTERNOON_PLAYGROUND //implicitly a String
+
+myWords = "new stuff"
+print(myWords)
+print(AFTERNOON_PLAYGROUND)
+myWords = "other words"
+
+var otherVariable = 7
+otherVariable = Int(3.952) //Casting a double as an Int
