6de656523a327ab3077d0425430b6b8d617eefb5
Adam Kinzer <adamkinzer915@gmail.com>
2015-12-03 13:54:20 -0700

    Tested loops.

--- Tutorial Playground.playground/Contents.swift
+++ Tutorial Playground.playground/Contents.swift
@@ -13,3 +13,48 @@ myWords = "other words"
 
 var otherVariable = 7
 otherVariable = Int(3.952) //Casting a double as an Int
+
+
+//Array - Java version:
+//int [] myNums = new int [4];
+//myNums[0] = 3;
+
+
+//Array - Swift version:
+var myNums = [Int]()
+myNums.append(3)
+myNums.append(2)
+myNums.append(45)
+myNums.append(35)
+print(myNums)
+
+
+//ArrayList - Java
+//ArrayList<String> myWords = ArrayList<String>();
+//myWor.add("stuff");
+
+var myWordsList = [String]()
+myWordsList.insert("stuff", atIndex: 0)
+
+
+//Java Array bigness = name.length
+//Swift
+print(myNums.count)
+print(myWordsList.count)
+
+//Java - for each or the enhanced for
+//for(Type variable : structure)
+// {...}
+//Swift - the for in loop
+//for ariable in structure
+// {...}
+for myNum in myNums
+{
+    print(myNum)
+}
+
+for(var myNumber = 0; myNumber < myNums.count; myNumber+=3)
+{
+    print(myNumber)
+}
+
